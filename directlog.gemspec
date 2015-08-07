b3e785ba89ae0010130a670b5362fb7b0ed38f1d
Wesley Conde <kerponeis@gmail.com>
2015-08-07 20:52:27 -0300

    fix to ruby version >= 2.0.0, < 2.1.0

--- directlog.gemspec
+++ directlog.gemspec
@@ -13,7 +13,7 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/wesleyskap/directlog"
   spec.license       = "MIT"
 
-  spec.required_ruby_version     = '>= 2.2.2'
+  spec.required_ruby_version     = '>= 2.0.0'
   spec.required_rubygems_version = '>= 1.8.11'
 
   spec.files         = `git ls-files -z`.split("\x0")
--- lib/directlog/freight.rb
+++ lib/directlog/freight.rb
@@ -1,6 +1,6 @@
 module Directlog
   class Freight < Base
-    def self.calculate(weight:, zip_code_destination:, amount:)
+    def self.calculate(weight: nil, zip_code_destination: nil, amount: nil)
       call(:calcular_frete, { 'peso' => weight, 'cepDestino' => zip_code_destination, 'valor' => amount }).result[:directlog][:consultafrete]
     end
   end
