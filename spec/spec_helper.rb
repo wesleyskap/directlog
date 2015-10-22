c7fd666ae265ebc69cb6fb74f0c0eb0f5e99ddb9
Wesley Conde <kerponeis@gmail.com>
2015-10-22 12:34:09 -0200

    add vcr config :allow_unused_http_interactions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@ require 'webmock'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
+  c.default_cassette_options = {
+    :allow_unused_http_interactions => false
+  }
 end
 
 RSpec.configure do |c|
